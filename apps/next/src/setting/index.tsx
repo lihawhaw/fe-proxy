ff33ae3fff0ae2f8225ab7bdce379f71599bfbd3
lihawhaw <lihawhaw@163.com>
2023-09-19 02:23:29 +0800

    clear

--- apps/next/src/setting/index.tsx
+++
@@ -1,6 +0,0 @@
-/* @refresh reload */
-import { render } from "solid-js/web";
-import App from "./App";
-import "../assets/styles/index.css";
-
-render(() => <App />, document.getElementById("app") as HTMLElement);
--- packages/ui/src/index.ts
+++
@@ -1,1 +0,0 @@
-export const foo = (value: string) => value || "foo";
